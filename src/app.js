b5bb1c60648d435ef95776c52a00cef4bff2b327
greyby <greyby.5@gmail.com>
2015-08-13 23:26:52 +0800

    add bootstrap

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
+require('bootstrap');
+
+require('bootstrap/dist/css/bootstrap.css');
+
 var Vue = require('vue')
 var Router = require('director').Router
 var app = new Vue(require('./app.vue'))
 var router = new Router()
+
 
 
 router.on('/:page', function (page) {
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,48 @@
+var webpack = require("webpack");
+
 module.exports = {
-  entry: "./src/app.js",
+  // entry: "./src/app.js",
+  entry: {
+    app: "./src/app.js",
+    vendors: ["jquery","bootstrap"]
+  },
+
+  // live reload
+  // entry: {
+  //   app: ['webpack/hot/dev-server','./src/app.js']
+  // },
+  // plugins: [
+  //   new webpack.HotModuleReplacementPlugin()
+  // ]
+
   output: {
     path: "./build",
     filename: "build.js"
   },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+      "window.jQuery": "jquery",
+      "root.jQuery": "jquery"
+    }),
+    new webpack.optimize.CommonsChunkPlugin("vendors", "vendor.bundle.js")
+  ],
+  // externals: {
+  //       // require("jquery") is external and available
+  //       //  on the global var jQuery
+  //       "jquery": "jQuery"
+  // },
+
   module: {
     loaders: [
       { test: /\.vue$/, loader: "vue-loader" },
+      { test: /\.css$/, loader: "style-loader!css-loader" }, // use ! to chain loaders
+      { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192' }, // inline base64 URLs for <=8k images, direct URLs for the rest
+      { test: /\.ttf$/,    loader: "file" },
+      { test: /\.eot$/,    loader: "file" },
+      { test: /\.svg$/,    loader: "file" },
+      { test: /\.(woff|woff2)$/, loader: 'url-loader?limit=100000' }
     ]
   },
   devtool: "source-map"
