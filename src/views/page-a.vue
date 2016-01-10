a68eb2f7769749a91a0234a22a533df8e978a70b
greyby <greyby.5@gmail.com>
2016-01-10 10:48:32 +0800

    bind dynamic class

--- src/views/page-a.vue
+++ src/views/page-a.vue
@@ -2,21 +2,17 @@
   <div class="container" >
   <validator name="validation1">
   <app-header v-bind:msg="msg"></app-header>
-      <div class="form-group">
+      <div class="form-group" :class="{ 'has-error': true }">
         <input type="text" class="form-control input-lg" v-model="mobile" placeholder="mobile" aria-label="mobile" v-validate:mobile="{ required: true, minlength: 5, maxlength: 12 }">
-        <div class="validate-error">
-            <p v-show="$validation1.mobile.required && $validation1.mobile.dirty">mobile required</p>
-            <p v-show="$validation1.mobile.minlength && $validation1.mobile.dirty">mobile is too short</p>
-            <p v-show="$validation1.mobile.maxlength && $validation1.mobile.dirty">mobile is too long</p>
-        </div>
+            <p class="help-block" v-show="$validation1.mobile.required && $validation1.mobile.dirty">mobile required</p>
+            <p class="help-block" v-show="$validation1.mobile.minlength && $validation1.mobile.dirty">mobile is too short</p>
+            <p class="help-block" v-show="$validation1.mobile.maxlength && $validation1.mobile.dirty">mobile is too long</p>
       </div>
       <div class="form-group">
         <input type="text" class="form-control input-lg" v-model="name" placeholder="name" aria-label="name" v-validate:name="{ required: true, minlength: 2, maxlength: 8 }">
-        <div class="validate-error">
-          <p v-show="$validation1.name.required && $validation1.name.dirty">name required</p>
-          <p v-show="$validation1.name.minlength && $validation1.name.dirty">name is too short</p>
-          <p v-show="$validation1.name.maxlength && $validation1.name.dirty">name is too long</p>
-        </div>
+          <p class="help-block" v-show="$validation1.name.required && $validation1.name.dirty">name required</p>
+          <p class="help-block" v-show="$validation1.name.minlength && $validation1.name.dirty">name is too short</p>
+          <p class="help-block" v-show="$validation1.name.maxlength && $validation1.name.dirty">name is too long</p>
       </div>
         
       <button type="submit" class="btn btn-primary btn-lg btn-block">Submit</button>
